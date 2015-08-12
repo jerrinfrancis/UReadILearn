b9003d4a6bcf30a309ec4f917d4a1f423d50ada5
jerrin0011 <jerrinfrancis001@gmail.com>
2015-08-13 00:30:44 +0530

    added navigation bar

--- index.php
+++ index.php
@@ -1,6 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-
+<title>UReadiLearn</title>
+<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 </head>
+<body>
+  <nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+      <div class="navbar-header">
+	<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+	<span>
+        <a class="navbar-brand" href="index.php" style="font-size:30px;color:#fff;font-family: 'Josefin Sans', sans-serif;">UReadILearn</a>
+	</span>
+      </div>
+    </div>
+  </nav>
+  
+
+</body>
 </html>
