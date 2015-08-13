85cb06a44897a9a6345ae20189db169aeaae38ca
jerrin0011 <jerrinfrancis001@gmail.com>
2015-08-13 23:43:58 +0530

    search

--- index.php
+++ index.php
@@ -7,10 +7,13 @@
   <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
   <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+  <link href='http://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
+  <link href='http://fonts.googleapis.com/css?family=Open+Sans:300' rel='stylesheet' type='text/css'>
+  <link href='http://fonts.googleapis.com/css?family=Josefin+Sans' rel='stylesheet' type='text/css'>
 </head>
 <body>
   <nav class="navbar navbar-inverse">
-    <div class="container-fluid">
+    <div class="container">
       <div class="navbar-header">
 	<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
         <span class="icon-bar"></span>
@@ -18,10 +21,27 @@
         <span class="icon-bar"></span>
       </button>
 	<span>
-        <a class="navbar-brand" href="index.php" style="font-size:30px;color:#fff;font-family: 'Josefin Sans', sans-serif;">UReadILearn</a>
+        <a class="navbar-brand" href="index.php" style="font-size:30px;color:#fff;font-family: 'Josefin Sans', sans-serif;">UReadiLearn</a>
+	</span>
+	<span>
+	  <a href="index.php">
+	    <img src="logo1.jpg" width="60" height="60">				
+	  </a>
 	</span>
       </div>
+      <div class="collapse navbar-collapse" id="myNavbar">
+	<form class="navbar-form navbar-left" role="search">
+	   <div class="form-group">
+	     <input type="text" class="form-control" placeholder="Search">
+	   </div>
+	   <button type="submit" class="btn btn-default">Submit</button>
+	</form>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+      </ul>
     </div>
+   </div>
   </nav>
   
 
